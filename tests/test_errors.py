f27403721199fa7f4685e60f7b2a15f7ccbe82e8
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-08-14 14:45:39 +0200

    Proper import

--- tests/test_errors.py
+++ tests/test_errors.py
@@ -1,13 +1,13 @@
 """Unit tests for the errors module."""
 
 import pytest
-from f8a_ingestion.utils import errors
+from f8a_ingestion.errors import F8AConfigurationException
 
 
 def test_f8a_configuration_exception():
     """A sort of dummy test the basic behaviour of F8AConfigurationException class."""
     e = F8AConfigurationException("some message")
-    assert e.message == "some message"
+    assert e.__str__() == "some message"
 
     with pytest.raises(F8AConfigurationException) as e:
         raise F8AConfigurationException("some message")
